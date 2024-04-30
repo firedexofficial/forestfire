3078325dc92ed0054fec4b280e0c18672fca3996
infra <infragf+github@gmail.com>
2024-04-29 21:26:35 -0400

    that's embarassing as heck

--- forest/core.py
+++ forest/core.py
@@ -1590,7 +1590,7 @@ class QuestionBot(PayBot):
         # if answer contains a valid email address, add it to maybe_email
         maybe_match = re.search(
             r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""",
-            answer,
+            answer.lower(),
         )
         # maybe_email is a re.match object, which returns only if there is a match.
         if maybe_match:
